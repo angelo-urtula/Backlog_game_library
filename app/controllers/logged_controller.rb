1b8a84af033ee6e257818be472c9b57ad1dd5cce
angelo <aurtula1@gmail.com>
2021-04-18 17:26:06 -0400

    added sign-up function that checks for unique name

--- app/controllers/logged_controller.rb
+++ app/controllers/logged_controller.rb
@@ -13,6 +13,19 @@ class LoggedController < ApplicationController
         logout!
     end
 
+    get '/signup' do
+        erb :'/logged/signup'
+    end
+
+    post '/signedup' do
+        signup(params[:username], params[:password])
+        redirect '/games'
+    end
+
+    get '/signup/failure' do
+        erb :'/logged/signup_failure'
+    end
+
     get '/games' do
         
         "You are logged in as #{session[:username]}"

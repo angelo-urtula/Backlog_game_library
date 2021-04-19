5c65d9bcd8c3eeaf0e41cc0843e92e8f3d134eb8
angelo <aurtula1@gmail.com>
2021-04-18 22:31:47 -0400

    added edit fxn with dynamic textboxes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,18 @@ class UsersController < ApplicationController
         end
     end
 
+    post '/profiles/:id' do
+        if !params[:user].keys.include?("game_ids")
+            params[:user]["game_ids"] = []
+        end
+
+        @user = User.find(params[:id])
+        @user.update(params["user"])
+        if !params["game"]["name"].empty? && !params["game"]["console"].empty? && !params["game"]["genre"].empty?
+            @user.games << Game.create(:name => params["game"]["name"], :console => params["game"]["console"], :genre => params["game"]["genre"])
+        
+        
+        end
+        redirect to "/profiles/#{params[:id]}"
+    end
 end

6bb2ba339129f80d13b4e3593ce7f44662ed240f
angelo <aurtula1@gmail.com>
2021-04-18 21:22:03 -0400

    added function to edit backlog library

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,19 @@ class UsersController < ApplicationController
         erb :'/user/index'
     end
 
-    get '/profile/:id' do
+    get '/profiles/:id' do
+        @user = User.find(params[:id])
         erb :'/user/show'
     end
+
+    get '/profiles/:id/edit' do
+        @user = User.find(params[:id])
+        @games = Game.all
+        if session[:username] == @user.username
+        erb :'/user/edit'
+        else
+            erb :'/user/warning'
+        end
+    end
+
 end
